63c70fef1ad09299482321ceac0fcc64d2af43fa
Daniil Meijel <115462899+AeroDynamicc@users.noreply.github.com>
2022-10-10 14:08:13 +0300

    Add files via upload

---
+++ h03d.py
@@ -0,0 +1,30 @@
+#Daniil Meijel
+#10.10.22
+#Harjutame kilpkonna
+import turtle
+
+#tekitame akna
+aken = turtle.Screen()
+aken.setup(300,300)
+aken.title("Ülesanne 3 - MM")
+
+
+
+k1 = turtle.Turtle()
+for i in range (3) :
+    k1.fd(100)
+    k1.rt(120)
+    k1.rt(120)
+
+k2 = turtle.Turtle()
+for i in range (3)
+    k2
+    
+    
+    
+
+
+
+
+
+turtle.exitonclick()
